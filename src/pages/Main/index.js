3e3c64d22524ace299a3ce10270d53467cb18115
KelvinLopes <kelvin7android@gmail.com>
2019-12-23 15:09:38 -0300

    Add function handleAddUser, add try,catch e finnaly

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -7,6 +7,8 @@ import AsyncStorage from '@react-native-community/async-storage';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import IconInfo from 'react-native-vector-icons/MaterialCommunityIcons';
 
+import api from '../../services/api';
+
 
 export default class Main extends Component {
 
@@ -34,6 +36,63 @@ async componentDidUpdate(){
   if(prevState.users !== this.setState.users) {
     AsyncStorage.setItem('users', JSON.stringify(users));
   }
+}
+
+handleAddUser = async () => {
+  this.setState({ loading: true });
+
+  try {
+    const { newUser, user } = this.state;
+    const alreadyUserLogin = users.find(us => us.login === newUser );
+    const { alreadyUserName } = users.find(us => us.name === newUser);
+
+    if(alreadyUserLogin || alreadyUserName ) {
+      throw new Error ('Você já salvou esse perfil.');
+    }
+
+    const response = await api.get(`users/${newUser}`);
+
+    const data = {
+      name: response.data.name,
+      login: response.data.login,
+      bio: response.data.bio,
+      avatar: response.data.avatar,
+    }
+
+    if(newUser !== response.data.login || newUser === '') {
+      throw new Error('Digite um perfil do Github');
+    }
+
+    this.setState({
+      users: [...users, data],
+      checkError: true,
+      sendErrorMessage: '',
+      newUser: '',
+    });
+
+    Alert.alert(
+      'Parabéns',
+      `O perfil de ${newUser} foi adicionado em sua lista.`,
+      [{ text: 'Fechar', onDismiss: () => {} }]
+    );
+
+  }catch(error) {
+
+    let textMessage = '';
+    if (error !== 'Você já salvou esse perfil.')
+    textMessage = 'Digite um perfil do Github.'
+    else textMessage = error;
+
+    this.setState({
+      checkError: false,
+      newUser: '',
+      sendErrorMessage: textMessage,
+    })
+
+  }finally {
+    this.setState({ loading: false, checkError: false });
+  }
+  Keyboard.dismiss();
 }
 
 //Auxilia na otimização do carregamento das informações ao fechar e abrir o app
